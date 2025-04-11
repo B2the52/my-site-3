ef017fdb4a3f54c4358a575242bbf40dc358fad9
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2025-04-11 21:30:39 +0000

    Initial commit

---
+++ src/pages/Home.c1dmp.js
@@ -0,0 +1,12 @@
+$w.onReady(function(){ 
+$w('#button1').onClick(()=>{
+    $w('#text4').text = "Wsg"
+    })
+$w('#button2').onClick(()=>{
+    $w('#text4').text = "Not much hbu"
+})
+$w('#button3').onClick(()=>{
+    
+})
+});
+
